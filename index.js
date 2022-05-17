7646217b23d9d67369757d0a3d536c12f456b657
Milena Marlim <milenamarlim.dev@gmail.com>
2022-05-17 11:28:22 -0300

    Commit com estilização botao hamburguer

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 const btnMobile = document.getElementById('btn_mobile')
 
-
-//Criando função que será ativada quando clicar nele
 function toggleMenu() {
     const nav = document.getElementById('nav')
     nav.classList.toggle('active')
-
 }
 
 btnMobile.addEventListener('click', toggleMenu)
 
-// const nav = document.getElementById('nav')
-// console.log(nav)
