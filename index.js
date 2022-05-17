f590359473cea1499e8eaf2be689255136b8abfb
Milena Marlim <milenamarlim.dev@gmail.com>
2022-05-17 11:05:15 -0300

    Commit com a construção do menu hamburguer e atualização item search cabeçalho

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-// AOS.init({
-//     duration: 1000;
-// });
-    
+const btnMobile = document.getElementById('btn_mobile')
+
+
+//Criando função que será ativada quando clicar nele
+function toggleMenu() {
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('active')
+
+}
+
+btnMobile.addEventListener('click', toggleMenu)
+
+// const nav = document.getElementById('nav')
+// console.log(nav)
